6734f046a3bb5cb00fb4f62e638c3fcaa2eb3d64
Theo Bulut <vertexclique@gmail.com>
2023-01-05 00:20:04 +0300

    Veri tipleri alistirmalari

--- A100_veri-tipleri-ve-sentaks_alistirma_1/src/main.rs
+++ A100_veri-tipleri-ve-sentaks_alistirma_1/src/main.rs
@@ -1,3 +1,24 @@
+// 🌋: Alistirma 1: Tipler arasi otomatik donusumler
+//
+// Rust'ta tipler arasi otomatik donusum yoktur.
+// Yazmadan once ne kullanacaginizi ve neye donustureceginizi
+// bilmeniz veya onceden verinin sigacagi bir veri tipi secmeniz gereklidir.
+
+fn carp(x: i16, y: i16) -> i16 {
+    x * y
+}
+
 fn main() {
-    println!("Hello, world!");
+    let x: i8 = 15;
+    let y: i16 = 1000;
+
+    // 1.Kodu derlenecek hale getirin.
+    println!("{x} * {y} = {}", carp(x, y));
+
+    // 2. Bu kod sizce derlenecek hale gelir mi?
+    // 3. Bu kod derlense de calisir mi?
+    let z: i32 = 12783891;
+    println!("{z} * {y} = {}", carp(z, y));
+
+    // 4. x ve y nin degerlerini f32, bool ve i128 gibi veri tipleriyle degistirip derleyin.
 }
--- A100_veri-tipleri-ve-sentaks_alistirma_2/src/main.rs
+++ A100_veri-tipleri-ve-sentaks_alistirma_2/src/main.rs
@@ -1,3 +1,79 @@
+// 🌋: Alistirma 2: Arrayler ve for donguleri
+//
+// Ornek 1: For donguleri asagidaki gibidir.
+
+// fn main() {
+//     let array = [10, 20, 30, 40];
+//     print!("Array'i iterate ediyoruz:");
+//     for n in array {
+//         print!(" {n}");
+//     }
+//     println!();
+//
+//     print!("Bir exclusive range'i iterate ediyoruz:");
+//     for i in 0..3 {
+//         print!(" {}", array[i]);
+//     }
+//     println!();
+//
+//     print!("Bir inclusive range'i iterate ediyoruz:");
+//     for i in 0..=3 {
+//         print!(" {}", array[i]);
+//     }
+//     println!();
+// }
+
+// 1. Matrisin transpozunu alan fonksiyonu yaziniz.
+// Bir matrisin sol ust kosesinden sag alt kosesine cizilen bir cizgide
+// matrisin elemanlarinin yer degistirilmesine matris transpoze denir.
+//
+// [ 1, 2, 3]        [ 1, 4, 7]
+// [ 4, 5, 6] ---->  [ 2, 5, 8]
+// [ 7, 8, 9]        [ 3, 6, 9]
+//
+// 2. Pretty print fonksiyonunu yazin.
+// Soyle bir matris verildiginde
+//
+// let matrix = [
+//     [101, 102, 103],
+//     [201, 202, 203],
+//     [301, 302, 303],
+// ];
+//
+// bunu bastirmasini istiyoruz:
+// ===========
+// 101 102 103
+// 201 202 203
+// 301 302 303
+// ===========
+// 3. Transpoze fonksiyonunu arrayler yerine slicelar kullanarak yazabilir miydik?
+// Odev... Yazin...
+// fn transpoze(matrix: &[&[i32]]) -> &[&[i32]] {
+// 4. Transpoze fonksiyonunuz kare olmayan matris icin de calisiyor mu?
+// 5. Bu transpoze fonksiyonunu unutmayin.
+// 6. Daha az memory kullanarak bu kodu yazabilir miydiniz? Dusunun.
+
+#![allow(unused_variables, dead_code)]
+
+fn transpoze(matrix: [[i32; 3]; 3]) -> [[i32; 3]; 3] {
+    todo!("Transpose fonksiyonunu yazin.")
+}
+
+fn pretty_print(matrix: &[[i32; 3]; 3]) {
+    todo!("Bir matrisi alip guzelce matris seklinde bastiran fonksiyonu yazin.")
+}
+
 fn main() {
-    println!("Hello, world!");
+    let matris = [
+        [101, 102, 103],
+        [201, 202, 203],
+        [301, 302, 303],
+    ];
+
+    println!("matris:");
+    pretty_print(&matris);
+
+    let transpoze = transpoze(matris);
+    println!("transpoze edilmis matris:");
+    pretty_print(&transpoze);
 }
